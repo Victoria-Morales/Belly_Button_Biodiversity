0818ce16d97e33c603d1080e40ec91463c683ed3
Victoria Morales <vcm41879@yahoo.com>
2021-02-20 15:13:11 -0800

    updated chart.js

--- charts.js
+++ charts.js
@@ -93,5 +93,40 @@ function buildCharts(sample) {
     };
     // 10. Use Plotly to plot the data with the layout. 
     Plotly.newPlot("bar", barData, barLayout);
+ 
+    // Start of deliverable 2 - bubble chart
+
+
+    // 1. Create the trace for the bubble chart.
+    var trace1 = [{
+      x: otuIds,
+      y: sampleValues,
+      mode: 'markers',
+      text: otuLabels,
+      marker: {
+        color: otuIds,
+        size: sampleValues,
+        colorscale:"Electric"
+      }
+    }];
+        
+    var bubbleData = [trace1
+   
+    ];
+
+    // 2. Create the layout for the bubble chart.
+    var bubbleLayout = {
+      title: "Bacteria Cultures Per Sample",
+      xaxis: {title: "OTU ID"},
+      margins: {t:10},
+      hovermode: 'closest',
+      height: 600,
+      width: 650
+           
+    };
+
+    // 3. Use Plotly to plot the data with the layout.
+    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
   });
+ 
 }
